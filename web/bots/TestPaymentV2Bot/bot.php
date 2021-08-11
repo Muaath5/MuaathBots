328a1b6456135fa19cafcce4d61e59bded8660eb
Muaath Alqarni <muaath1428@hotmail.com>
2021-08-11 20:37:27 +0300

    Fixing SimpleBotAPI Error

--- web/bots/TestPaymentV2Bot/bot.php
+++ web/bots/TestPaymentV2Bot/bot.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace MuaathBots;
 
-require('../../../vendor/autoload.php');
+require 'vendor/autoload.php';
 
 use SimpleBotAPI\TelegramBot;
 use SimpleBotAPI\UpdatesHandler;
--- web/bots/TestPaymentV2Bot/webhook.php
+++ web/bots/TestPaymentV2Bot/webhook.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace MuaathBots;
 
-require '../../../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 use MuaathBots\TestPaymentV2Bot;
 use SimpleBotAPI\TelegramBot;
