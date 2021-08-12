3d6800f373d75c7569693105cc7cd8c7463a42d1
Muaath Alqarni <muaath1428@hotmail.com>
2021-08-12 15:33:10 +0300

    Testing, Fixing redirection

--- web/bots/TestPaymentV2Bot/webhook.php
+++ web/bots/TestPaymentV2Bot/webhook.php
@@ -3,27 +3,29 @@ declare(strict_types=1);
 
 namespace MuaathBots;
 
-require '/app/vendor/autoload.php';
+require '/home/muaath/websites/muaath-bots/vendor/autoload.php';
 
 use MuaathBots\TestPaymentV2Bot;
 use SimpleBotAPI\TelegramBot;
 
 $BotDir = basename(__DIR__);
+$Token = $_GET['token'];
 
 # Check auth
-if ($_GET['token'] != getenv('PAYMENT_BOT_TOKEN'))
+if ($Token != getenv('PAYMENT_BOT_TOKEN'))
 {
-    include $_SERVER['DOCUMENT_ROOT'] . "/bots/webhook-unauthorized.php?bot={$BotDir}";
+    header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-unauthorized.php?bot={$BotDir}");
     exit;
 }
+
+$Bot = new TelegramBot(getenv('PAYMENT_BOT_TOKEN'));
 
 # Reading the update from Telegram to the webhook
 $Update = json_decode(file_get_contents('php://input'));
 
 if (!empty($Update))
 {
-    # Create the Bot
-    $Bot = new TelegramBot(getenv('PAYMENT_BOT_TOKEN'));
+    # Set UpdatesHandler for the bot
     $Bot->SetUpdatesHandler(new TestPaymentV2Bot($Bot, getenv('PAYMENT_BOT_PROVIDER_TOKEN'), getenv('PAYMENT_BOT_LOGS_CHAT_ID')));
 
     # Process the update
@@ -32,5 +34,6 @@ if (!empty($Update))
 else
 {
     # Show the admin settings of the webhook, Contains: Webhook info, Delete webhook, Set webhook
-    include "../webhook-settings.php?token={$token}&bot={$BotDir}";
+    header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-settings.php?token={$Token}&bot={$BotDir}");
+    exit;
 }
