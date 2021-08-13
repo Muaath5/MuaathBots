47db4de193276972b7f953460e5f4663833778ed
Muaath Alqarni <muaath1428@hotmail.com>
2021-08-13 21:43:06 +0300

    Fix redirection

--- web/bots/RemoveInlineButtonsBot/webhook.php
+++ web/bots/RemoveInlineButtonsBot/webhook.php
@@ -9,7 +9,7 @@ use MuaathBots\RemoveInlineButtonsBot;
 use SimpleBotAPI\TelegramBot;
 
 $BotDir = basename(__DIR__);
-$Token = $_GET['token'];
+$Token = $_REQUEST['token'];
 
 # Check auth
 if ($Token != getenv('REMOVE_INLINE_BUTTONS_BOT_TOKEN'))
@@ -19,8 +19,9 @@ if ($Token != getenv('REMOVE_INLINE_BUTTONS_BOT_TOKEN'))
 }
 
 $Bot = new TelegramBot($Token, new RemoveInlineButtonsBot(getenv('REMOVE_INLINE_BUTTONS_BOT_LOGS_CHAT_ID')));
+$Bot->OnWebhookUpdate();
 
-if (!$Bot->OnWebhookUpdate())
+if (isset($_REQUEST['s']))
 {
     # Show the admin settings of the webhook, Contains: Webhook info, Delete webhook, Set webhook
     header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-settings.php?token={$Token}&bot={$BotDir}");
--- web/bots/TestPaymentV2Bot/webhook.php
+++ web/bots/TestPaymentV2Bot/webhook.php
@@ -9,7 +9,7 @@ use MuaathBots\TestPaymentV2Bot;
 use SimpleBotAPI\TelegramBot;
 
 $BotDir = basename(__DIR__);
-$Token = $_GET['token'];
+$Token = $_REQUEST['token'];
 
 # Check auth
 if ($Token != getenv('PAYMENT_BOT_TOKEN'))
@@ -19,9 +19,9 @@ if ($Token != getenv('PAYMENT_BOT_TOKEN'))
 }
 
 $Bot = new TelegramBot(getenv('PAYMENT_BOT_TOKEN'), new TestPaymentV2Bot(getenv('PAYMENT_BOT_PROVIDER_TOKEN'), getenv('PAYMENT_BOT_LOGS_CHAT_ID')));
+$Bot->OnWebhookUpdate();
 
-
-if (!$Bot->OnWebhookUpdate())
+if (isset($_REQUEST['s']))
 {
     # Show the admin settings of the webhook, Contains: Webhook info, Delete webhook, Set webhook
     header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-settings.php?token={$Token}&bot={$BotDir}");
