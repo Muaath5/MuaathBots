88fc6b2e08026b985101bbbcece782fc6ec6ac23
Muaath Alqarni <muaath1428@hotmail.com>
2021-10-07 20:06:34 +0300

    Fixing RemoveInlineButtonsBot webhook

--- web/bots/RemoveInlineButtonsBot/webhook.php
+++ web/bots/RemoveInlineButtonsBot/webhook.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace MuaathBots;
 
-require '/app/vendor/autoload.php';
+require_once '/app/vendor/autoload.php';
 
 use MuaathBots\RemoveInlineButtonsBot;
 use SimpleBotAPI\BotSettings;
 use SimpleBotAPI\TelegramBot;
 
-$Bot = new TelegramBot($Token, new RemoveInlineButtonsBot(), BotSettings::Import(__DIR__ . '/settings.json'));
+$Bot = new TelegramBot(getenv('REMOVE_INLINE_BUTTONS_BOT_TOKEN'), new RemoveInlineButtonsBot(), BotSettings::Import(__DIR__ . '/settings.json'));
 if (!$Bot->OnWebhookUpdate())
 {
     echo 'false';
