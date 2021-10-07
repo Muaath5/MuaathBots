f9cc3c9174967839e3df3bf3cec47f64405e0942
Muaath Alqarni <muaath1428@hotmail.com>
2021-10-07 19:58:56 +0300

    Add webhook authorization

--- web/bots/RemoveInlineButtonsBot/webhook.php
+++ web/bots/RemoveInlineButtonsBot/webhook.php
@@ -9,26 +9,8 @@ use MuaathBots\RemoveInlineButtonsBot;
 use SimpleBotAPI\BotSettings;
 use SimpleBotAPI\TelegramBot;
 
-$BotDir = basename(__DIR__);
-$Token = $_REQUEST['token'];
-
-# Check auth
-if ($Token != getenv('REMOVE_INLINE_BUTTONS_BOT_TOKEN'))
-{
-    header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-unauthorized.php?bot={$BotDir}");
-    exit;
-}
-
-
-if (isset($_REQUEST['s']))
-{
-    # Show the admin settings of the webhook, Contains: Webhook info, Delete webhook, Set webhook
-    header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-settings.php?token={$Token}&bot={$BotDir}");
-    exit;
-}
-
 $Bot = new TelegramBot($Token, new RemoveInlineButtonsBot(), BotSettings::Import(__DIR__ . '/settings.json'));
-if (!$Bot->OnWebhookUpdate(file_get_contents('php://input')))
+if (!$Bot->OnWebhookUpdate())
 {
     echo 'false';
 }
--- web/bots/TestPaymentV2Bot/webhook.php
+++ web/bots/TestPaymentV2Bot/webhook.php
@@ -3,28 +3,13 @@ declare(strict_types=1);
 
 namespace MuaathBots;
 
-require '/app/vendor/autoload.php';
+require_once '/app/vendor/autoload.php';
 
 use MuaathBots\TestPaymentV2Bot;
 use SimpleBotAPI\BotSettings;
 use SimpleBotAPI\TelegramBot;
 
-$BotDir = basename(__DIR__);
-$Token = $_REQUEST['token'];
-
-# Check auth
-if ($Token != getenv('PAYMENT_BOT_TOKEN'))
-{
-    header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-unauthorized.php?bot={$BotDir}");
-    exit;
-}
-
-if (isset($_REQUEST['s']))
-{
-    # Show the admin settings of the webhook, Contains: Webhook info, Delete webhook, Set webhook
-    header('Location: ' . "https://muaath-bots.herokuapp.com/bots/webhook-settings.php?token={$Token}&bot={$BotDir}");
-    exit;
-}
+$Token = getenv('PAYMENT_BOT_TOKEN');
 
 $Bot = new TelegramBot($Token, new TestPaymentV2Bot(), BotSettings::Import(__DIR__ . '/settings.json'));
 if (!$Bot->OnWebhookUpdate())
